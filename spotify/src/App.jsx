4b0f0aeb58487cb37afacfbd06df7f36eeb6a03a
Lakshya Singh <sinlakshya17@gmail.com>
2026-07-24 17:10:59 +0530

    Ui

--- spotify/src/App.jsx
+++ spotify/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Sidebar from './Componenets/Sidebar'
+import Player from './Componenets/Player'
+import Display from './Componenets/Display'
+
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
         <div className='h-[90%] flex'>
             <Sidebar />
+            <Display />
         </div>
+           <Player />
     </div>
   )
 }
